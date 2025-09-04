7a47608246917d9a07803e29d4d5b148958f1c11
lucasfrgabriel <lucasfrgabriel@gmail.com>
2025-09-04 20:06:30 -0300

    chore: esqueleto do sistema em java spring boot

---
+++ src/main/java/br/com/folhapagamento/calculo_folha_pagamento/CalculoFolhaPagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.folhapagamento.calculo_folha_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoFolhaPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoFolhaPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/folhapagamento/calculo_folha_pagamento/CalculoFolhaPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.folhapagamento.calculo_folha_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoFolhaPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
