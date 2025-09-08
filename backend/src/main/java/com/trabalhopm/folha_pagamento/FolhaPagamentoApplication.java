8d2e5ec7c583dbbdaa03d6e1e925f9d9c1d4b9ce
lucasfrgabriel <lucasfrgabriel@gmail.com>
2025-09-07 21:39:28 -0300

    esqueleto do sistema em java spring boot

---
+++ backend/src/main/java/com/trabalhopm/folha_pagamento/FolhaPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.trabalhopm.folha_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolhaPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolhaPagamentoApplication.class, args);
+	}
+
+}
