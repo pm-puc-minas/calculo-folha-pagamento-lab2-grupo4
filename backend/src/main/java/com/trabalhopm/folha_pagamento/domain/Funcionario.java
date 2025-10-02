2a2bbf5a219c4f53b5db74ab34acceaf4bbe1129
lucasfrgabriel <lucasfrgabriel@gmail.com>
2025-10-02 18:21:09 -0300

    refactor: classe funcionario

--- backend/src/main/java/com/trabalhopm/folha_pagamento/domain/Funcionario.java
+++ backend/src/main/java/com/trabalhopm/folha_pagamento/domain/Funcionario.java
@@ -1,68 +1,47 @@
 package com.trabalhopm.folha_pagamento.domain;
 
 import java.time.LocalDate;
+
+import com.trabalhopm.folha_pagamento.utils.ValidaCPF;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Funcionario {
+
+    @NotBlank(message = "O nome não pode estar em branco")
     private String nome;
+
+    @NotBlank(message = "O CPF não pode estar em branco")
     private String cpf;
+
+    @NotBlank(message = "O cargo não pode estar em branco")
     private String cargo;
+
+    @PastOrPresent(message = "Digite uma data de admissão válida")
     private LocalDate dataAdmissao;
+
+    @PositiveOrZero(message = "O número de dependentes não pode ser negativo")
     private byte numeroDeDependentes;
+
+    @Min(0)
+    @Max(100)
     private byte nivelInsalubridade;
 
-    @Setter
     private boolean temPericulosidade;
 
-    @Setter
     private Financeiro financeiro;
 
-    public void setNome(String nome) {
-        if (nome == null || nome.isEmpty()) {
-            throw new IllegalArgumentException("O nome não pode ser vazio");
-        }
-        this.nome = nome;
-    }
-
-    public void setCpf(String cpf) {
-        if (cpf == null || cpf.isEmpty()) {
-            throw new IllegalArgumentException("O CPF não pode ser vazio");
+    public void setCPF(String cpf) {
+        if(!ValidaCPF.isCPF(cpf)) {
+            throw new IllegalArgumentException("CPF Invalido");
         }
         this.cpf = cpf;
-    }
-
-    public void setCargo(String cargo) {
-        if (cargo == null || cargo.isEmpty()) {
-            throw new IllegalArgumentException("O cargo não pode ser vazio");
-        }
-        this.cargo = cargo;
-    }
-
-    public void setDataAdmissao(LocalDate dataAdmissao) {
-        if (dataAdmissao == null || dataAdmissao.isAfter(LocalDate.now())) {
-            throw new IllegalArgumentException("A data de admissão não é válida");
-        }
-        this.dataAdmissao = dataAdmissao;
-    }
-
-    public void setNumeroDeDependentes(byte numeroDeDependentes) {
-        if (numeroDeDependentes < 0) {
-            throw new IllegalArgumentException("Número de dependentes inválido");
-        }
-        this.numeroDeDependentes = numeroDeDependentes;
-    }
-
-    public void setNivelInsalubridade(byte nivelInsalubridade) {
-        if (nivelInsalubridade >= 0 && nivelInsalubridade <= 100) {
-            this.nivelInsalubridade = nivelInsalubridade;
-        } else {
-            throw new IllegalArgumentException("Nível de insalubridade inválido");
-        }
     }
 }
