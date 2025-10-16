7273e6863aa35016682f9f89a74124c9f3ac1561
lucasfrgabriel <lucasfrgabriel@gmail.com>
2025-10-16 19:29:55 -0300

    feat: mapear entidades no banco de dados

--- backend/src/main/java/com/trabalhopm/folha_pagamento/domain/Financeiro.java
+++ backend/src/main/java/com/trabalhopm/folha_pagamento/domain/Financeiro.java
@@ -1,8 +1,9 @@
 package com.trabalhopm.folha_pagamento.domain;
-import lombok.Setter;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 import jakarta.validation.constraints.Max;
@@ -14,19 +15,37 @@ import jakarta.validation.constraints.PositiveOrZero;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Financeiro {
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "financeiro")
+public class Financeiro implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
     @NotNull(message = "O salário bruto não pode ser nulo")
     @PositiveOrZero(message = "O salário bruto deve ser maior que zero")
+    @Column(name = "'salario_bruto")
     private BigDecimal salarioBruto;
 
     @PositiveOrZero(message = "As horas trabalhadas por dia devem ser maiores que zero")
+    @Column(name = "horas_trabalhadas_dia")
     private double horasTrabalhadasDia;
 
     @Min(value = 0, message = "Os dias trabalhados na semana devem ser entre 0 e 7")
     @Max(value = 7, message = "Os dias trabalhados na semana devem ser entre 0 e 7")
+    @Column(name = "dias_trabalhados_semana")
     private byte diasTrabalhadosSemana;
 
     @PositiveOrZero(message = "O valor diário do vale transporte não pode ser negativo")
+    @Column(name = "valor_diario_VT")
     private BigDecimal valorDiarioValeTransporte;
+
+    @OneToOne
+    @JoinColumn(name = "funcionario_id")
+    private Funcionario funcionario;
 }
--- backend/src/main/java/com/trabalhopm/folha_pagamento/domain/Funcionario.java
+++ backend/src/main/java/com/trabalhopm/folha_pagamento/domain/Funcionario.java
@@ -1,20 +1,30 @@
 package com.trabalhopm.folha_pagamento.domain;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import com.trabalhopm.folha_pagamento.domain.enums.NivelInsalubridade;
 import com.trabalhopm.folha_pagamento.utils.ValidaCPF;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Funcionario {
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @NotBlank(message = "O nome não pode estar em branco")
     private String nome;
@@ -25,16 +35,26 @@ public class Funcionario {
     @NotBlank(message = "O cargo não pode estar em branco")
     private String cargo;
 
+    @Column(name = "data_admissao")
     @PastOrPresent(message = "Digite uma data de admissão válida")
     private LocalDate dataAdmissao;
 
+    @Column(name = "numero_de_dependentes")
     @PositiveOrZero(message = "O número de dependentes não pode ser negativo")
     private byte numeroDeDependentes;
 
+    @Column(name = "nivel_insalubridade")
+    @Enumerated(EnumType.STRING)
     private NivelInsalubridade nivelInsalubridade;
 
+    @Column(name = "tem_periculosidade")
     private boolean temPericulosidade;
 
+    @Column(name = "ta_de_ferias")
+    private boolean deFerias;
+
+    @OneToOne
+    @JoinColumn(name = "financeiro_id")
     private Financeiro financeiro;
 
     public void setCPF(String cpf) {
